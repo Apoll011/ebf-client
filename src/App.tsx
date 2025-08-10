a7cfec53d04462f649e9b915508138041a2e308e
Apoll011 <tiagorobotik@gmail.com>
2025-08-10 21:14:58 -0100

    Refactored app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthScreen from "./pages/login.tsx";
+import DashboardPage from "./pages/Dashboard.tsx";
+import {AuthProvider} from "./api/useAuth.tsx";
+import StudentRegistration from "./pages/StudentRegister.tsx";
+import StudentInfo from "./pages/Student.tsx";
+import StudentList from "./pages/StudentList.tsx";
+
+
+export function App() {
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={ <DashboardPage/> } />
+                    <Route path="/login" element={<AuthScreen />} />
+                    <Route path="/register" element={<StudentRegistration />} />
+                    <Route path="/student/:studentId" element={<StudentInfo />} />
+                    <Route path="/list" element={<StudentList />} />
+                    <Route path="*" element={<div>Page Not Found</div>} />                </Routes>
+            </Router>
+        </AuthProvider>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthScreen from "./pages/login.tsx";
-import DashboardPage from "./pages/Dashboard.tsx";
-import {AuthProvider} from "./api/useAuth.tsx";
 import {StrictMode} from "react";
 import './index.css'
 import {createRoot} from "react-dom/client";
-import StudentRegistration from "./pages/StudentRegister.tsx";
-import StudentInfo from "./pages/Student.tsx";
+import {App} from "./App.tsx";
 
-function App() {
-    return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={ <DashboardPage/> } />
-                    <Route path="/login" element={<AuthScreen />} />
-                    <Route path="/register" element={<StudentRegistration />} />
-                    <Route path="/student/:studentId" element={<StudentInfo />} />
-                    <Route path="*" element={<div>Page Not Found</div>} />                </Routes>
-            </Router>
-        </AuthProvider>
-    );
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
