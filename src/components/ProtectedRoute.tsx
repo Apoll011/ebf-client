937e992e30385884e8205be5dee2a14bb3ecd96f
Apoll011 <tiagorobotik@gmail.com>
2025-08-13 15:36:36 -0100

    Added new ProtectedRoute

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,29 @@
+import { Navigate } from "react-router-dom";
+import {useAuth} from "../hooks/useAuth.tsx";
+import type { JSX } from "react";
+
+interface ProtectedRouteProps {
+    children: JSX.Element;
+}
+
+export function ProtectedRoute({ children }: ProtectedRouteProps) {
+    const { isAuthenticated, user, isLoading } = useAuth();
+
+    if (isLoading) {
+        return (
+            <div className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]" role="status">
+                <span  className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Carregando...</span>
+            </div>
+        );
+    }
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (user?.role === "viewer") {
+        return <Navigate to="/screensaver" replace />;
+    }
+
+    return children;
+}
--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,28 +1,21 @@
 import Header from "../components/Header.tsx";
-import {useAuth} from "../hooks/useAuth.tsx";
-import {useNavigate} from "react-router-dom";
 import React from "react";
+import {ProtectedRoute} from "../components/ProtectedRoute.tsx";
 
 export const MainLayout = ({children}: {children: React.ReactNode}) => {
-    const { isAuthenticated, isLoading, user } = useAuth();
-
-    const navigate = useNavigate();
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
     return (
-      <div className="flex flex-col min-h-screen bg-gray-100">
-          <Header />
-          <div className="min-h-screen  mb-4">
-              {children}
+      <ProtectedRoute>
+          <div className="flex flex-col min-h-screen bg-gray-100">
+              <Header />
+              <div className="min-h-screen  mb-4">
+                  {children}
+              </div>
+              <footer className="w-full  flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-gray p-6 bg-white shadow-sm border-t border-gray-500 text-center md:justify-between">
+                  <p className="font-normal text-blue-gray-500 text-sm">
+                      &copy; 2025 <strong>@Embrace</strong> by Tiago Inês &nbsp;|&nbsp; <em>1 Coríntios 10:31</em> — Portanto, quer comais, quer bebais, ou façais qualquer outra coisa, fazei tudo para a glória de Deus.
+                  </p>
+              </footer>
           </div>
-          <footer className="w-full justify-center  flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-gray p-6 bg-white shadow-sm border-t border-gray-500 text-center md:justify-between">
-              <p className="font-normal text-blue-gray-500 text-sm">
-                  &copy; 2025 <strong>@Embrace</strong> by Tiago Inês &nbsp;|&nbsp; <em>1 Coríntios 10:31</em> — Portanto, quer comais, quer bebais, ou façais qualquer outra coisa, fazei tudo para a glória de Deus.
-              </p>
-          </footer>
-      </div>
+      </ProtectedRoute>
     );
 };
