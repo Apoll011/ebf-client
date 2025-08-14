f82b380fa22752f68d79e58244ff54d07a74de33
Apoll011 <tiagorobotik@gmail.com>
2025-08-14 10:59:53 -0100

    Added background on glass mode

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -10,12 +10,12 @@ export const MainLayout = ({children}: {children: React.ReactNode}) => {
 
     return (
       <ProtectedRoute>
-          <div className="flex flex-col min-h-screen bg-gray-100">
+          <div className={`flex flex-col min-h-screen bg-gray-100 ${glassMode ? 'bg-[url(\'/wallpaper.webp\')] bg-cover bg-no-repeat bg-fixed' : ''}`}>
               <Header key={trigger} />
                   <div className={`min-h-screen mb-4 ${glassMode ? 'mt-4' : 'mt-0'}`}>
                   {children}
               </div>
-              <footer className={`${glassMode ? 'glass-card mx-5 mb-2 top-5' :'w-full bg-white'} flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-gray p-6 shadow-sm border-t border-gray-500 text-center md:justify-between`}>
+              <footer className={`${glassMode ? 'glass-card mx-5 mb-2 top-5 text-white' :'w-full bg-white'} flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-gray p-6 shadow-sm border-t border-gray-500 text-center md:justify-between`}>
                   <p className="font-normal text-blue-gray-500 text-sm">
                       &copy; 2025 <strong>@Embrace</strong> by Tiago Inês &nbsp;|&nbsp; <em>1 Coríntios 10:31</em> — Portanto, quer comais, quer bebais, ou façais qualquer outra coisa, fazei tudo para a glória de Deus.
                   </p>
